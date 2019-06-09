84819bfdd9f78aeb3dbad24612c8e32963080482
Maximilian <maximilian.sudmann@rwth-aachen.de>
2019-06-09 15:56:05 +0200

    :lipstick: Adding default colors

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#fbf1c7", "#282828" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#282828", "#b8bb26" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
